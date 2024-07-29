13c061269ca33392891f013ade1a4640f48cb040
Juan Gonon <juanfegonon27@gmail.com>
2024-07-29 15:58:31 -0600

    view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,11 @@
-import { Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
+import { NothingSelectedView } from '../view'
 
 export function JournalPage () {
   return (
     <JournalLayout>
-      <Typography>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate inventore similique nobis magni est molestias enim, maiores sed deleniti esse minima pariatur quam! Repellendus sunt hic dolor laudantium maxime soluta.
-      </Typography>
+
+      <NothingSelectedView />
     </JournalLayout>
   )
 }
