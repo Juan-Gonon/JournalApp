2832081b0d178ffb5906bfda493fa87189b5daa4
Juan Gonon <juanfegonon27@gmail.com>
2024-07-29 10:05:29 -0600

    form login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import { Grid, Typography } from '@mui/material'
+import { Google } from '@mui/icons-material'
+import { Button, Grid, Link as LinkUI, TextField, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 export function LoginPage () {
   return (
@@ -22,7 +24,65 @@ export function LoginPage () {
         }}
       >
         <Typography variant='h5'>
-          Login
+          <form>
+            <Grid container>
+              <Grid
+                item xs={12} sx={{
+                  marginTop: 2
+                }}
+              >
+                <TextField label='correo' type='email' placeholder='correo@google.com' fullWidth />
+              </Grid>
+              <Grid
+                item xs={12} sx={{
+                  marginTop: 2
+                }}
+              >
+                <TextField label='password' type='email' placeholder='correo@google.com' fullWidth />
+              </Grid>
+
+              <Grid
+                container
+                spacing={2}
+                sx={{
+                  marginBottom: 2,
+                  marginTop: 1
+                }}
+              >
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                >
+                  <Button variant='contained' fullWidth>Login</Button>
+                </Grid>
+
+                <Grid
+                  item
+                  xs={12}
+                  sm={6}
+                >
+                  <Button variant='contained' fullWidth>
+                    <Google />
+                    <Typography sx={{
+                      marginLeft: 1
+                    }}
+                    >Google
+                    </Typography>
+                  </Button>
+                </Grid>
+
+              </Grid>
+
+              <Grid container direction='row' justifyContent='end'>
+                <LinkUI component={Link} color='inherit' to='/auth/register'>
+                  Crear una cuenta
+                </LinkUI>
+
+              </Grid>
+
+            </Grid>
+          </form>
         </Typography>
 
       </Grid>
