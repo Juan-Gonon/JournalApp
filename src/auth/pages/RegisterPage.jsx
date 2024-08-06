a0ce314b636eb547e5e9680b5aa69113ae2f24a3
Juan Gonon <juanfegonon27@gmail.com>
2024-08-06 15:24:04 -0600

    register form

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,32 +1,71 @@
 import { Button, Grid, Link as LinkUI, TextField, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { AuthLayout } from '../layout/AuthLayout'
+import { useForm } from '../../hooks'
+
+const formData = {
+  email: 'juan@gmail.com',
+  password: '123456',
+  displayName: 'Juan'
+}
 
 export function RegisterPage () {
+  const { displayName, formState, email, password, onInputChange } = useForm(formData)
+
+  const onSubmitForm = (e) => {
+    e.preventDefault()
+
+    console.log(formState)
+  }
+
   return (
     <AuthLayout title='Crear cuenta'>
-      <form>
+      <form onSubmit={onSubmitForm}>
         <Grid container>
           <Grid
             item xs={12} sx={{
               marginTop: 2
             }}
           >
-            <TextField label='Nombre completo' type='text' placeholder='nombre' fullWidth />
+            <TextField
+              label='Nombre completo'
+              type='text'
+              placeholder='nombre'
+              fullWidth
+              name='displayName'
+              value={displayName}
+              onChange={onInputChange}
+            />
           </Grid>
           <Grid
             item xs={12} sx={{
               marginTop: 2
             }}
           >
-            <TextField label='correo' type='email' placeholder='correo@google.com' fullWidth />
+            <TextField
+              label='correo'
+              type='email'
+              placeholder='correo@google.com'
+              fullWidth
+              name='email'
+              value={email}
+              onChange={onInputChange}
+            />
           </Grid>
           <Grid
             item xs={12} sx={{
               marginTop: 2
             }}
           >
-            <TextField label='password' type='email' placeholder='correo@google.com' fullWidth />
+            <TextField
+              label='password'
+              type='password'
+              placeholder='correo@google.com'
+              fullWidth
+              name='password'
+              value={password}
+              onChange={onInputChange}
+            />
           </Grid>
 
           <Grid
@@ -42,7 +81,7 @@ export function RegisterPage () {
               xs={12}
               sm={6}
             >
-              <Button variant='contained' fullWidth>Crear cuenta</Button>
+              <Button type='submit' variant='contained' fullWidth>Crear cuenta</Button>
             </Grid>
 
           </Grid>
