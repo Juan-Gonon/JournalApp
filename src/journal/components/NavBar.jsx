4d3df2f26b5e22da29f813c4940609163ec6ba94
Juan Gonon <juanfegonon27@gmail.com>
2024-08-08 14:53:48 -0600

    logout de firebase

--- src/journal/components/NavBar.jsx
+++ src/journal/components/NavBar.jsx
@@ -1,8 +1,15 @@
 import { LogoutOutlined, MenuOutlined } from '@mui/icons-material'
 import { AppBar, Grid, IconButton, Toolbar, Typography } from '@mui/material'
+import { useDispatch } from 'react-redux'
+import { startLogout } from '../../store/auth'
 
 // eslint-disable-next-line react/prop-types
 export function NavBar ({ drawerWidth = 240 }) {
+  const dispatch = useDispatch()
+  const onLogout = () => {
+    console.log('onLogout')
+    dispatch(startLogout())
+  }
   return (
     <AppBar
       position='fixed' sx={{
@@ -25,7 +32,7 @@ export function NavBar ({ drawerWidth = 240 }) {
           alignItems='center'
         >
           <Typography variant='h6' noWrap component='div'>JournalApp</Typography>
-          <IconButton color='error'>
+          <IconButton color='error' onClick={onLogout}>
             <LogoutOutlined />
           </IconButton>
 
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,4 +1,4 @@
-import { loginWithEmailPassword, registerUserEmailPassword, singInWithGoogle } from '../../firebase/providers'
+import { loginWithEmailPassword, logoutFirebase, registerUserEmailPassword, singInWithGoogle } from '../../firebase/providers'
 import { checkingCredentials, login, logout } from './'
 
 export const checkingAuthentication = (email, password) => {
@@ -40,5 +40,13 @@ export const startLoginWithEmailPassword = ({ email, password }) => {
     if (!ok) return dispatch(logout({ errorMessage }))
 
     dispatch(login({ uid, displayName, email, photoURL }))
+  }
+}
+
+export const startLogout = () => {
+  return async (dispatch) => {
+    await logoutFirebase()
+
+    dispatch(logout({}))
   }
 }
