9813e2b207e89726c00a01f3fc7f0b39dbd84b6b
Juan Gonon <juanfegonon27@gmail.com>
2024-08-28 09:58:27 -0600

    delete image

--- test/helpers/fileUpload.test.js
+++ test/helpers/fileUpload.test.js
@@ -1,5 +1,14 @@
+import { v2 as cloudinary } from 'cloudinary'
 import { describe, expect, it } from 'vitest'
 import { fileUpload } from '../../src/helpers/fileUpload'
+
+// dashboard admin
+cloudinary.config({
+  cloud_name: 'drwyq8qqc',
+  api_key: '618882734825676',
+  api_secret: 'yGUbNbrsXhfwJZZ-JwWLHIq7_gc',
+  secure: true
+})
 
 describe('Prueba en fileUpload', () => {
   it('debe de subir el archivo correctamente a cloudinary', async () => {
@@ -9,7 +18,13 @@ describe('Prueba en fileUpload', () => {
     const file = new File([blob], 'foto.jpg')
     const url = await fileUpload(file)
 
+    // console.log(url)
     expect(typeof url).toBe('string')
+
+    const segments = url.split('/')
+    const imgId = segments[segments.length - 1].replace('.jpg', '')
+
+    await cloudinary.api.delete_all_resources([imgId])
   })
 
   it('debe de retornar null', async () => {
